d0042c9e501ae9f26215aa98eaab10b2e9c16f35
VIckie <kiki_cool1212@hotmail.com>
2018-10-26 15:27:33 -0400

    Atelier 11: Commencement du code FAIT: - Rajout des classes manquante
    TODO:
    - Rajouter le code de base
    - Coder les nouvelles methodes

---
+++ app/src/main/java/ca/cours5b5/vickielanglois/donnees/Serveur.java
@@ -0,0 +1,25 @@
+package ca.cours5b5.vickielanglois.donnees;
+
+import java.util.Map;
+
+public class Serveur extends SourceDeDonnees {
+
+    private Serveur(){}
+
+    private static final Serveur instance;
+
+    public static Serveur getInstance(){
+        return null;
+    }
+
+
+    @Override
+    public Map<String, Object> chargerModele(String cheminSauvegarde) {
+        return null;
+    }
+
+    @Override
+    public void sauvegarderModele(String cheminSauvegarde, Map<String, Object> objetJson) {
+
+    }
+}
